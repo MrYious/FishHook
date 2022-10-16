91eb2c79bd228f7d2223d14ecf516e9ae2573c75
MrYious <rosariomark37@gmail.com>
2022-10-16 17:03:00 +0800

    feat: added main tabs

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,9 @@
 import AboutTab from './AboutTab';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import HomeTab from './HomeTab';
 import InfoTab from './InfoTab';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
@@ -8,28 +11,62 @@ const Tab = createBottomTabNavigator();
 const Main = ({ navigation }) => {
   return (
     <Tab.Navigator tabBarShowLabel={()=>{return null}}
+      initialRouteName='Home'
+      backBehavior={'none'}
       screenOptions={{
         headerShown: false,
-        tabBarShowLabel: false,
+        // tabBarShowLabel: false,
       }}
     >
       <Tab.Screen
-        name="Home"
-        component={HomeTab}
-        // options={{
-        //   tabBarLabel: 'Home',
-        //   tabBarIcon: ({ color, size }) => (
-        //     <MaterialCommunityIcons name="home" color={color} size={size} />
-        //   ),
-        // }}
-      />
-      <Tab.Screen
         name="Info"
         component={InfoTab}
+        options={{
+          tabBarLabel: 'Info',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name={'info-outline'} size={30} color={'black'}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Camera"
+        component={AboutTab}
+        options={{
+          tabBarLabel: 'Camera',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name={'camera'} size={25} color={'black'}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={HomeTab}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name={'home'} size={25} color={'black'}/>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Gallery"
+        component={AboutTab}
+        options={{
+          tabBarLabel: 'Gallery',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name={'picture-o'} size={25} color={'black'}/>
+          ),
+        }}
       />
       <Tab.Screen
         name="About"
         component={AboutTab}
+        options={{
+          tabBarLabel: 'About',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name={'question-circle'} size={25} color={'black'}/>
+          ),
+        }}
       />
     </Tab.Navigator>
   );
