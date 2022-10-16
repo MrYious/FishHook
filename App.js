8d93cd905375d320e58390f4bf0fbbee905e147c
MrYious <rosariomark37@gmail.com>
2022-10-16 17:13:43 +0800

    feat: change main header title

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const App = () => {
           <Stack.Screen
             name="Main"
             component={Main}
-            options={{title: 'Main'}}
+            options={{title: 'FishHook'}}
           />
         </Stack.Navigator>
       </NavigationContainer>
